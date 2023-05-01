d3881a466eb910aaee18870b1ad5b75fb3ca9352
João Carlos Pontes <jcarlospontesf@gmail.com>
2023-05-01 20:13:09 -0300

    att nome

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Não acredito que finalmente deu certo
+          Atividade CI/CD
         </p>
         <a
           className="App-link"
